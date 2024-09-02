a61a66ec5a2d5d91d7912407bee7aba2631d4d18
Bogdan <listopad.bogdan@bk.ru>
2024-09-02 22:55:21 +0300

    add configurations for Breed and Species models

---
+++ backend/src/PetFinder.Infrastructure/Configurations/SpeciesConfiguration.cs
@@ -0,0 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PetFinder.Domain.Models;
+using PetFinder.Domain.Shared;
+
+namespace PetFinder.Infrastructure.Configurations;
+
+public class SpeciesConfiguration: IEntityTypeConfiguration<Species>
+{
+    public void Configure(EntityTypeBuilder<Species> builder)
+    {
+        builder.HasKey(s => s.Id);
+
+        builder.Property(s => s.Id)
+            .HasConversion(
+                id => id.Value,
+                value => SpeciesId.Create(value));
+
+        builder.HasMany(s => s.Breeds)
+            .WithOne(b => b.Species);
+
+        builder.Property(s => s.Title)
+            .HasMaxLength(Constants.Species.MaxTitleLength)
+            .IsRequired();
+    }
+}
