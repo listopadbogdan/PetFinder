754feccfdcb229ba03975524a6456bc44e5730de
Bogdan <listopad.bogdan@bk.ru>
2024-09-02 22:57:59 +0300

    Add SpeciesBreedObject to Pet model

---
+++ backend/src/PetFinder.Domain/Models/Volunteer/SpeciesBreedObject.cs
@@ -0,0 +1,21 @@
+using CSharpFunctionalExtensions;
+using PetFinder.Domain.Shared;
+
+namespace PetFinder.Domain.Models;
+
+public record SpeciesBreedObject
+{
+    private SpeciesBreedObject()
+    {
+    }
+
+    public SpeciesId SpeciesId { get; private set; } = default!;
+    public BreedId BreedId { get; private set; }
+
+    public static Result<SpeciesBreedObject> Create(SpeciesId speciesId, BreedId breedId)
+        => Result.Success(new SpeciesBreedObject()
+        {
+            SpeciesId = speciesId,
+            BreedId = breedId
+        });
+}
