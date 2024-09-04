1255bc3c9acecf6ccd3798cfe7f3600e57a2e16b
listopadbogdan <listopad.bogdan@bk.ru>
2024-09-04 18:00:14 +0300

    Add static create method for volunteer

--- backend/src/PetFinder.Domain/Models/Volunteer/Volunteer.cs
+++ backend/src/PetFinder.Domain/Models/Volunteer/Volunteer.cs
@@ -1,16 +1,34 @@
-using PetFinder.Domain.Shared;
+using CSharpFunctionalExtensions;
 
 namespace PetFinder.Domain.Models;
 
-public class Volunteer : Entity<VolunteerId>
+public class Volunteer : Shared.Entity<VolunteerId>
 {
-    private readonly List<AssistanceDetails> _assistanceDetails = new();
-    private readonly List<Pet> _pets = new();
-    private readonly List<SocialNetwork> _socialNetworks = new();
-
+    private readonly List<AssistanceDetails> _assistanceDetails = default!;
+    private readonly List<Pet> _pets = default!;
+    private readonly List<SocialNetwork> _socialNetworks = default!;
     private Volunteer(VolunteerId id)
         : base(id)
     {
+    }
+
+    private Volunteer(
+        VolunteerId id,
+        PersonName personName,
+        PhoneNumber phoneNumber,
+        List<SocialNetwork> socialNetworks,
+        List<AssistanceDetails> assistanceDetails,
+        int experienceYears,
+        string description) : base(id)
+    {
+        PersonName = personName;
+        PhoneNumber = phoneNumber;
+        ExperienceYears = experienceYears;
+        Description = description;
+        
+        _socialNetworks = socialNetworks;
+        _assistanceDetails = assistanceDetails;
+        _socialNetworks = [];
     }
 
     public PersonName PersonName { get; private set; } = default!;
@@ -24,4 +42,25 @@ public class Volunteer : Entity<VolunteerId>
     public int PetsFoundHomeCount => Pets.Count(p => p.HelpStatus == HelpStatusPet.FoundHome);
     public int PetsLookingForHomeCount => Pets.Count(p => p.HelpStatus == HelpStatusPet.LookingForHome);
     public int PetsOnTreatmentCount => Pets.Count(p => p.HelpStatus == HelpStatusPet.OnTreatment);
+
+    public static Result<Volunteer> Create(
+        VolunteerId id,
+        PersonName personName,
+        PhoneNumber phoneNumber,
+        IEnumerable<SocialNetwork>? socialNetworks,
+        IEnumerable<AssistanceDetails>? assistanceDetails,
+        int experienceYears,
+        string description)
+    {
+        var volunteer = new Volunteer(
+            id: id,
+            personName: personName,
+            phoneNumber: phoneNumber,
+            socialNetworks: socialNetworks?.ToList() ?? [],
+            assistanceDetails: assistanceDetails?.ToList() ?? [],
+            experienceYears: experienceYears,
+            description: description
+        );
+        return Result.Success(volunteer);
+    }
 }
