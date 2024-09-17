b69e11e68321467963bf27fa3d9520df0168692a
Bogdan <listopad.bogdan@bk.ru>
2024-09-17 20:15:50 +0300

    Modify ResponseExtension

--- backend/src/PetFinder.API/Extensions/ResponseExtensions.cs
+++ backend/src/PetFinder.API/Extensions/ResponseExtensions.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using PetFinder.API.Response;
 using PetFinder.Domain.Shared;
 using PetFinder.Domain.SharedKernel;
 
@@ -6,17 +7,52 @@ namespace PetFinder.API.Extensions;
 
 public static class ResponseExtensions
 {
-    public static IActionResult ToResponse(this Error error)
+    public static ActionResult ToResponse(this Error error)
     {
-        var statusCode = error.ErrorType switch
+        var statusCode = GetStatusCodeForErrorType(error.ErrorType);
+
+        var envelope = Envelope.Error(error.ToErrorList());
+
+        return new ObjectResult(envelope)
+        {
+            StatusCode = statusCode
+        };
+    }
+
+    public static ActionResult ToResponse(this ErrorList errors)
+    {
+        if (errors.Any())
+            return new ObjectResult(Envelope.Error(errors))
+            {
+                StatusCode = StatusCodes.Status500InternalServerError
+            };
+
+        var distinctErrorTypes = errors
+            .Select(x => x.ErrorType)
+            .Distinct()
+            .ToList();
+
+        var statusCode = distinctErrorTypes.Count > 1
+            ? StatusCodes.Status500InternalServerError
+            : GetStatusCodeForErrorType(distinctErrorTypes.First());
+
+        var envelope = Envelope.Error(errors);
+
+        return new ObjectResult(envelope)
+        {
+            StatusCode = statusCode
+        };
+    }
+
+    private static int GetStatusCodeForErrorType(ErrorType errorType)
+    {
+        return errorType switch
         {
             ErrorType.Validation => StatusCodes.Status400BadRequest,
             ErrorType.NotFound => StatusCodes.Status404NotFound,
             ErrorType.Conflict => StatusCodes.Status409Conflict,
             ErrorType.Failure => StatusCodes.Status500InternalServerError,
-            _ => StatusCodes.Status500InternalServerError,
+            _ => StatusCodes.Status500InternalServerError
         };
-
-        return new ObjectResult(error) { StatusCode = statusCode };
     }
 }
