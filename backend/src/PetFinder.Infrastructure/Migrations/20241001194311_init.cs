2f57aa4c2da7f7efd15206f0213b25af9128df4d
Bogdan <listopad.bogdan@bk.ru>
2024-10-01 23:45:42 +0300

    reset migrations

--- backend/src/PetFinder.Infrastructure/Migrations/20241001194311_init.cs
+++ backend/src/PetFinder.Infrastructure/Migrations/20241001194311_init.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 namespace PetFinder.Infrastructure.Migrations
 {
     /// <inheritdoc />
-    public partial class Init : Migration
+    public partial class init : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
@@ -29,14 +29,16 @@ namespace PetFinder.Infrastructure.Migrations
                 {
                     id = table.Column<Guid>(type: "uuid", nullable: false),
                     experience_years = table.Column<int>(type: "integer", nullable: false),
+                    social_networks = table.Column<string>(type: "text", nullable: false),
+                    assistance_details = table.Column<string>(type: "text", nullable: false),
+                    is_deleted = table.Column<bool>(type: "boolean", nullable: false),
+                    deleted_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
                     description = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: false),
                     email = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: false),
                     first_name = table.Column<string>(type: "character varying(32)", maxLength: 32, nullable: false),
                     last_name = table.Column<string>(type: "character varying(32)", maxLength: 32, nullable: false),
                     middle_name = table.Column<string>(type: "character varying(32)", maxLength: 32, nullable: true),
-                    phone_number = table.Column<string>(type: "character varying(16)", maxLength: 16, nullable: false),
-                    assistance_details = table.Column<string>(type: "jsonb", nullable: false),
-                    social_networks = table.Column<string>(type: "jsonb", nullable: false)
+                    phone_number = table.Column<string>(type: "character varying(16)", maxLength: 16, nullable: false)
                 },
                 constraints: table =>
                 {
@@ -77,11 +79,13 @@ namespace PetFinder.Infrastructure.Migrations
                     weight = table.Column<double>(type: "double precision", nullable: false),
                     height = table.Column<double>(type: "double precision", nullable: false),
                     owner_phone = table.Column<string>(type: "character varying(16)", maxLength: 16, nullable: false),
-                    birth_date = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    birth_date = table.Column<DateOnly>(type: "date", nullable: false),
                     is_castrated = table.Column<bool>(type: "boolean", nullable: false),
                     is_vaccinated = table.Column<bool>(type: "boolean", nullable: false),
                     help_status = table.Column<int>(type: "integer", nullable: false),
                     created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    is_deleted = table.Column<bool>(type: "boolean", nullable: false),
+                    deleted_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
                     volunteer_id = table.Column<Guid>(type: "uuid", nullable: false),
                     city = table.Column<string>(type: "character varying(64)", maxLength: 64, nullable: false),
                     country = table.Column<string>(type: "character varying(64)", maxLength: 64, nullable: false),
@@ -111,6 +115,8 @@ namespace PetFinder.Infrastructure.Migrations
                     id = table.Column<Guid>(type: "uuid", nullable: false),
                     path = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: false),
                     is_main = table.Column<bool>(type: "boolean", nullable: false),
+                    is_deleted = table.Column<bool>(type: "boolean", nullable: false),
+                    deleted_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
                     pet_id = table.Column<Guid>(type: "uuid", nullable: false)
                 },
                 constraints: table =>
