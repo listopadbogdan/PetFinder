28b3104da7af147e0b543123773f0578263094a9
Bogdan <listopad.bogdan@bk.ru>
2024-10-01 21:29:35 +0300

    Confiugre GetHashCode and Equals in base entity class

--- backend/src/PetFinder.Domain/SharedKernel/Entity.cs
+++ backend/src/PetFinder.Domain/SharedKernel/Entity.cs
@@ -3,4 +3,7 @@ namespace PetFinder.Domain.SharedKernel;
 public abstract class Entity<T>(T id)
 {
     public T Id { get; } = id;
+
+    public override int GetHashCode() => HashCode.Combine(GetType().GetHashCode(), Id);
+    public override bool Equals(object? obj) => obj is Entity<T> entity && entity.Id!.Equals(Id);
 }
